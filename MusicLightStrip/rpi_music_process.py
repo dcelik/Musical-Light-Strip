9ab7983cd3813a8ef219af18c404d5959c6e8de5
Jacob Riedel <jr112394@gmail.com>
2017-04-09 14:35:44 -0400

    process instead of pool

--- MusicLightStrip/rpi_music_process.py
+++ MusicLightStrip/rpi_music_process.py
@@ -1,6 +1,6 @@
 
 import numpy
-from multiprocessing import Pool
+from multiprocessing import Process
 import alsaaudio, time, audioop
 try:
     import RPi.GPIO as GPIO
@@ -12,17 +12,23 @@ import alsaaudio, time, audioop
 
 audio_signal = []
 
-def listen_audio_input():
+def listen_audio_input(input):
+    while True:
+        l,data = input.read()
+        if l:
+            # Return the maximum of the absolute value of all samples in a fragment.
+            print audioop.max(data, 2)
+            audio_signal = audioop.max(data, 2)
+        time.sleep(.001)
+
+def create_audio_process():
     inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
     inp.setchannels(1)
     inp.setrate(8000)
     inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
     inp.setperiodsize(8)
 
-    while True:
-        l,data = inp.read()
-        if l:
-            # Return the maximum of the absolute value of all samples in a fragment.
-            print audioop.max(data, 2)
-            audio_signal = audioop.max(data, 2)
-        time.sleep(.001)
+    p = Process(target = listen_audio_input, args=(inp))
+    p.start()
+    p.join()
+
